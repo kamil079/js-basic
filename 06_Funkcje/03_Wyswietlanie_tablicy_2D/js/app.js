a68273039d3d894d686f01c756ded38a4a26c13b
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-27 09:09:44 +0100

    print 2D arr

--- 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
+++ 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
@@ -1,11 +1,22 @@
-const users = [["Jaydn Humphries", "Ayda Orozco"], ["Sanjeev Wilkinson", "Jorge Markham"]];
+const users = [
+    ["Jaydn Humphries", "Ayda Orozco"],
+    ["Sanjeev Wilkinson", "Jorge Markham"]
+];
 
 /**
  * Write your code below!
  */
 
 
+function print2DArray(array) {
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < array.length; j++) {
+            console.log(array[i][j])
+        }
+    }
+}
 
+print2DArray(users);
 
 /**
  * Do NOT modify code below!
