74f122f8dc99b77795dcc2c9ade0ade256d4712f
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-27 21:38:12 +0100

    first duplicate

--- 06_Funkcje/05_Powtarzajaca_sie_wartosc/js/app.js
+++ 06_Funkcje/05_Powtarzajaca_sie_wartosc/js/app.js
@@ -1,5 +1,19 @@
-const numbers = [2, 4, 5, 2, 3, 5, 1, 2, 4];
+const numbers = [2, 8, 5, 2, 3, 5, 4, 2, 4];
 
 /**
  * Write your code below!
  */
+
+function indexOfRepeatedValue(array) {
+  let firstIndex;
+  for (let i = 0; i <= array.length; i++) {
+    for (let j = i + 1; j <= array.length; j++) {
+      if (array[i] == array[j]) {
+        firstIndex = array[j];
+        return firstIndex;
+      }
+    }
+  }
+}
+
+console.log(indexOfRepeatedValue(numbers));
