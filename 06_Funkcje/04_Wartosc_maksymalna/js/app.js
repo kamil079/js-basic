12e6676cd16a8c434c4a51496c362479d28b8a8b
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-27 09:15:57 +0100

    max val

--- 06_Funkcje/04_Wartosc_maksymalna/js/app.js
+++ 06_Funkcje/04_Wartosc_maksymalna/js/app.js
@@ -3,3 +3,12 @@ const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
 /**
  * Write your code below!
  */
+
+
+function maxFromArray(numbers) {
+    numbers.sort((a,b) => a - b)
+    let result = numbers.slice(-1)
+    console.log(result)
+}
+
+maxFromArray(randomNumbers);
