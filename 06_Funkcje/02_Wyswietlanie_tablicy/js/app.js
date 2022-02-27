f2cf34e8e1858882b48f912d3a3c6cf4da008d4b
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-27 09:04:54 +0100

    print arr

--- 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
+++ 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
@@ -4,6 +4,15 @@ const people = ["John", "Ive", "Donna", "Chris"];
  * Write your code below!
  */
 
+
+function printArray(array) {
+    for(let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    } 
+}
+
+printArray(people);
+
 /**
  * Do NOT modify code below!
  */
